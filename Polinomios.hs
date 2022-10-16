ffa978a188e2bed81896dfddc034765a2f994793
kiko-serra <franciscopimentelserra@gmail.com>
2022-10-16 18:31:14 +0100

    Keep trying to finish parsing

--- Polinomios.hs
+++ Polinomios.hs
@@ -1,12 +1,20 @@
 import Data.Char
 
-data Polinomio = Mult Int | Var Char | Exp Int
+--bibliotecas que podem ser usadas
+--Data.List Data.String Data.Char
+
+data Monomio = Mult Int | Var Char | Exp Int
 -- polinomio do tipo numero * variavel ^ expoente
 --normalize polynomials
-scanner :: String -> [Polinomio]
+scanner :: String -> [Monomio]
 scanner "" = []
-scanner (x:xs)
-            | isDigit x = Mult (digitToInt x) : scanner xs
-            | isAlpha x = Var x : scanner xs
-            --por condicao para se tiver variavel depois temos que ler o expoente
-            | otherwise = scanner xs
+scanner (x : xs)
+  | isDigit x = Mult (digitToInt x) : scanner xs
+  | isAlpha x = Var x : scanner xs
+  | isDigit (head xs) = Exp (digitToInt (head xs)) : scanner (tail xs)
+  | otherwise = scanner xs
+
+--objetivo era guardar logo o primeiro monomio e depois ir guardando os restantes
+scanner2 :: String -> [Monomio]
+scanner2 "" = []
+scanner2 (x : y: z : w : t : xs) = Mult (digitToInt x) : Var z : Exp (digitToInt t) : scanner2 xs
