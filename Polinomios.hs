6a87fa4302cf98afc0f1cbfa33b0f7a04324da1a
Ptozin <93348093+Ptozin@users.noreply.github.com>
2022-10-17 21:30:39 +0100

    Update Polinomios.hs

--- Polinomios.hs
+++ Polinomios.hs
@@ -1,20 +1,84 @@
-import Data.Char
+import Data.Char   
 
---bibliotecas que podem ser usadas
---Data.List Data.String Data.Char
+data Variables = Var { variable :: Char, degree :: Int} deriving (Show, Eq, Read)
 
-data Monomio = Mult Int | Var Char | Exp Int
--- polinomio do tipo numero * variavel ^ expoente
---normalize polynomials
-scanner :: String -> [Monomio]
-scanner "" = []
-scanner (x : xs)
-  | isDigit x = Mult (digitToInt x) : scanner xs
-  | isAlpha x = Var x : scanner xs
-  | isDigit (head xs) = Exp (digitToInt (head xs)) : scanner (tail xs)
-  | otherwise = scanner xs
+data Monomial = Mono { coefficient :: Int, variables :: [Variables]} deriving (Show, Eq, Read)
 
---objetivo era guardar logo o primeiro monomio e depois ir guardando os restantes
-scanner2 :: String -> [Monomio]
-scanner2 "" = []
-scanner2 (x : y: z : w : t : xs) = Mult (digitToInt x) : Var z : Exp (digitToInt t) : scanner2 xs
+data Polynomial = Poly { monomials :: [Monomial]} deriving (Show, Eq, Read)
+
+-- Split into separate Monomials -------
+
+_addSpaceToBreak :: String -> String 
+_addSpaceToBreak [] = []
+_addSpaceToBreak (c:str)
+                | isDigit c && isLetter (head str) = [c] ++ ['*'] ++  _addSpaceToBreak str
+                | isLetter c && isLetter (head str) = [c] ++ ['*'] ++ _addSpaceToBreak str
+                | c == ' ' = _addSpaceToBreak str
+                | c == '^' && head(str) == '-' = [c] ++ [head str] ++ _addSpaceToBreak (tail str)
+                | c == '-' = [' '] ++ [c] ++ _addSpaceToBreak str
+                | c == '+' = [' '] ++ _addSpaceToBreak str 
+                | otherwise = [c] ++ _addSpaceToBreak str
+
+
+_breakString :: String -> [String]
+_breakString str = words (_addSpaceToBreak (str ++ [' '])) -- Space added to not fail functions
+
+
+-- Split into separate Monomials -------
+
+-- n
+-- x
+-- 2*x
+-- x^2
+getCoefficient :: String -> Int
+getCoefficient str 
+            | isDigit (head str) = read (takeWhile isDigit str) :: Int
+            | otherwise = 1
+
+getDegree :: String -> Int
+getDegree str 
+            | isDigit (head str) = read (takeWhile isDigit str) :: Int
+            | otherwise = error "Degree not valid"
+
+coefficientFreeString :: String -> String
+-- coefficientFreeString str = dropWhile isDigit str
+coefficientFreeString (c:str) 
+            | isDigit c = coefficientFreeString str
+            | c == '*' = coefficientFreeString str
+            | otherwise = [c] ++ str
+
+
+parseForVariable :: String -> String
+parseForVariable [] = []
+parseForVariable (c:str) 
+        | c == '^' = parseForVariable str
+        | otherwise = [c] ++ parseForVariable str
+
+getVariable :: String -> Variables
+getVariable (var:degree)
+            | isLetter var && null degree = Var var 1
+            | isLetter var && isDigit (head degree) = Var var (read degree :: Int)
+            | otherwise = error "Degree not valid"
+
+getVariables :: [String] -> [Variables]
+getVariables [] = []
+getVariables (str:strs) = [getVariable (parseForVariable str)] ++ getVariables strs
+            
+
+string2Monomial :: String -> Monomial
+string2Monomial str
+            | length str == 1 && isDigit (head str) = Mono (digitToInt (head str)) [Var 'N' 0]
+            | length str == 1 && isLetter (head str) = Mono 1 [Var (head str) 1]
+            | otherwise = Mono (getCoefficient str) (getVariables (_breakString (coefficientFreeString str)))
+
+
+auxString2Monomial :: [String] -> [Monomial]
+auxString2Monomial [] = []
+auxString2Monomial (str:strs) = [string2Monomial str] ++ auxString2Monomial strs
+
+string2Polynomial :: String -> Polynomial
+string2Polynomial str = Poly (auxString2Monomial (_breakString str))
+            
+
+
+-- Example: "1 + x^2 + y + 2*y"
