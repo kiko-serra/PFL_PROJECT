63040be9ef0367f85a0aab427972b3da459306e8
Ptozin <93348093+Ptozin@users.noreply.github.com>
2022-10-17 23:29:32 +0100

    transform poly into readable string

--- Polinomios.hs
+++ Polinomios.hs
@@ -1,31 +1,39 @@
 import Data.Char   
 
--- Example: "1 + 0 + x + 2*x + x^2 + 2*x^2 - 1 - x^-2"
+-- Example: "1 + 0 + x + 2*x + x^2 + 2*x^2 -1 -x -x^-2"
+
+-- Variable 'N' refers to no variable --> 2 = 2*N^0
+
+-- Data -----------------------------------------------
 
 data Variables = Var { variable :: Char, degree :: Int} deriving (Show, Eq, Read)
 
 data Monomial = Mono { coefficient :: Int, variables :: [Variables]} deriving (Show, Eq, Read)
 
-data Polynomial = Poly { monomials :: [Monomial]} deriving (Show, Eq, Read)
+type Polynomial = [Monomial]
 
--- Split into separate Monomials -------
+-------------------------------------------------------
 
-_addSpaceToBreak :: String -> String 
-_addSpaceToBreak [] = []
-_addSpaceToBreak (c:str)
-                | isDigit c && isLetter (head str) = [c] ++ ['*'] ++  _addSpaceToBreak str
-                | isLetter c && isLetter (head str) = [c] ++ ['*'] ++ _addSpaceToBreak str
-                | c == ' ' = _addSpaceToBreak str
-                | c == '^' && head(str) == '-' = [c] ++ [head str] ++ _addSpaceToBreak (tail str)
-                | c == '-' = [' '] ++ [c] ++ _addSpaceToBreak str
-                | c == '+' = [' '] ++ _addSpaceToBreak str 
-                | otherwise = [c] ++ _addSpaceToBreak str
+-- Add spaces to break into multiple strings ----------
 
-
-_breakString :: String -> [String]
-_breakString str = words (_addSpaceToBreak (str ++ [' '])) -- Space added to not fail functions
+addSpace :: String -> String 
+addSpace [] = []
+addSpace (c:str)
+                | isDigit c && isLetter (head str) = [c] ++ ['*'] ++  addSpace str
+                | isLetter c && isLetter (head str) = [c] ++ ['*'] ++ addSpace str
+                | c == ' ' = addSpace str
+                | c == '^' && head(str) == '-' = [c] ++ [head str] ++ addSpace (tail str)
+                | c == '-' = [' '] ++ [c] ++ addSpace str
+                | c == '+' = [' '] ++ addSpace str 
+                | otherwise = [c] ++ addSpace str
 
 
+breakString :: String -> [String]
+breakString str = words (addSpace (str ++ [' '])) -- Space added to not fail functions
+
+-------------------------------------------------------
+
+-- Get Coefficient from the string
 getCoefficient :: String -> Int
 getCoefficient str 
             | isDigit (head str) = read (takeWhile isDigit str) :: Int
@@ -69,17 +77,44 @@ string2Monomial str
             | length str == 2 && head str == '-' && isDigit (head (tail str)) = Mono (negate (digitToInt (head (tail str)))) [Var 'N' 0]
             | length str == 2 && head str == '-' && isLetter (head (tail str)) = Mono (negate 1) [Var (head (tail str)) 1]
             | isDigit (head str) && digitToInt (head str) == 0 = Mono 0 []
-            | head str == '-' = Mono (negate (getCoefficient (tail str))) (getVariables (_breakString (coefficientFreeString (tail str))))
-            | otherwise = Mono (getCoefficient str) (getVariables (_breakString (coefficientFreeString str)))
+            | head str == '-' = Mono (negate (getCoefficient (tail str))) (getVariables (breakString (coefficientFreeString (tail str))))
+            | otherwise = Mono (getCoefficient str) (getVariables (breakString (coefficientFreeString str)))
 
 
-auxString2Monomial :: [String] -> [Monomial]
-auxString2Monomial [] = []
-auxString2Monomial (str:strs) = [string2Monomial str] ++ auxString2Monomial strs
+auxString2Ponomial :: [String] -> [Monomial]
+auxString2Ponomial [] = []
+auxString2Ponomial (str:strs) = [string2Monomial str] ++ auxString2Ponomial strs
 
 string2Polynomial :: String -> Polynomial
-string2Polynomial str = Poly (auxString2Monomial (_breakString str))
+string2Polynomial str = auxString2Ponomial (breakString str)
             
 
 
--- Example: "1 + x^2 + y + 2*y"
+-- Transform Polynomial into a readable one -----------
+
+
+variables2String :: [Variables] -> String
+variables2String [] = []
+variables2String (var:remainder) 
+            | degree var == 0 = variables2String remainder
+            | otherwise = "*" ++ [variable var] ++ ['^'] ++ show (degree var) ++ variables2String remainder
+            
+
+monomial2String :: Monomial -> String
+monomial2String mono 
+            | (coefficient mono > 0) = "+" ++ show (coefficient mono) ++ variables2String (variables mono)
+            | otherwise = show (coefficient mono) ++ variables2String (variables mono)
+
+polynomial2String :: Polynomial -> String
+polynomial2String [] = []
+polynomial2String (mono:poly) 
+            | null poly = monomial2String mono
+            | otherwise = monomial2String mono ++ " " ++ polynomial2String poly
+
+-------------------------------------------------------
+
+main = do   
+        putStrLn "Insert the polynomial: "
+        asked <- getLine
+        putStrLn ("Result: " ++ polynomial2String (string2Polynomial asked))
+
