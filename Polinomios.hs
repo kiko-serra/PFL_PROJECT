00c865e95bea7f9c8783212ccd6c7a2c11007ec5
Ptozin <93348093+Ptozin@users.noreply.github.com>
2022-10-20 10:33:01 +0100

    Update Polinomios.hs

--- Polinomios.hs
+++ Polinomios.hs
@@ -1,9 +1,17 @@
 import Data.Char   
 import Data.List 
 
+-- ADICIONAR O PACKAGE QUICKCHECK
+-- import Test.QuickCheck
+-- prop :: [Int] -> Bool
+-- prop l = reverse (reverse l) == l
+-- terminal fazer::: -> quickCheck prop
+
 -- Example: "1 + 0 + x + 2*x + x^2 + 2*x^2 -1 -x -x^-2 + x*y + 0*x*y"
 
 -- Variable 'N' refers to no variable --> 2 = 2*N^0
+
+---- TODO -------->   make "x*x" something possible
 
 -- Data -----------------------------------------------
 
@@ -111,16 +119,33 @@ removeExponents :: [Variables] -> [Variables]
 removeExponents [] = []
 removeExponents vars = [v | v <- vars, (degree v) /= 0]
 
+clearVar :: Variables -> [Variables] -> [Variables]
+clearVar var vars = [v | v <-vars, (variable var) /= (variable v)]
+
+filterVar :: Variables -> [Variables] -> [Variables]
+filterVar var vars = [v | v <-vars, (variable var) == (variable v)]
+
+sumDeg :: [Variables] -> Variables
+sumDeg vars = Var (variable (head vars)) (sum[degree var | var <- vars])
+
+sumDegFromSameExp :: [Variables] -> [Variables]
+sumDegFromSameExp [] = []
+sumDegFromSameExp (var:vars) = [sumDeg(filterVar var (var:vars)) ] ++ sumDegFromSameExp (clearVar var (var:vars))
+
+filterExponents :: [Variables] -> [Variables]
+filterExponents [] = []
+filterExponents vars = sumDegFromSameExp(removeExponents (vars))
+
+
 clearPolynomial :: Polynomial -> Polynomial
 clearPolynomial [] = []
-clearPolynomial poly = [Mono (coefficient mono) (removeExponents (variables mono)) | mono <-poly]
+clearPolynomial poly = [Mono (coefficient mono) (filterExponents (variables mono)) | mono <-poly]
 
 
 -- Sort (decreasing) and combine similar monomials
 
 sumCoefficients :: Polynomial -> Int
 sumCoefficients poly = sum [coefficient mono | mono <- poly]
-
 
 equalVariables :: [Variables] -> [Variables] -> Bool
 equalVariables slave tester = (sort slave) == (sort tester)
@@ -131,13 +156,9 @@ clearUsedMonomials mono poly = [eq_mono | eq_mono <- poly, not (equalVariables (
 filteredMonomials :: Monomial -> Polynomial -> Polynomial
 filteredMonomials mono poly = [eq_mono | eq_mono <- poly, equalVariables (variables mono) (variables eq_mono)]
 
-
-
 auxNormalizePoly :: Polynomial -> Polynomial
 auxNormalizePoly [] = []
-auxNormalizePoly (mono:poly) = [Mono (sumCoefficients (mono:(filteredMonomials mono poly))) (variables mono)] ++ auxNormalizePoly (clearUsedMonomials mono poly)
-
-
+auxNormalizePoly (mono:poly) = [Mono (sumCoefficients (mono:(filteredMonomials mono poly))) (variables mono)] ++ auxNormalizePoly (clearUsedMonomials mono poly)           
 
 -- Transform Polynomial into a readable one -----------
 
@@ -202,55 +223,29 @@ addPolynomials str1 str2 = parsePolynomial2String( string2Polynomial (concatenat
 
 --- c) multiply polynomials ----------------------------
 
--- faz tuplos de vars, de seguida multiplica-os
-
-tupleVars :: [(Variables, Variables)] -> [Variables]
-tupleVars [] = []
-tupleVars (tuple:tuples)
-                | (variable (fst tuple)) == (variable (snd tuple)) = [Var (variable (fst tuple)) (degree (fst tuple) + degree (snd tuple))] ++ tupleVars tuples
-                | otherwise = [fst tuple] ++ [snd tuple]  ++ tupleVars tuples
-multVars :: [Variables] -> [Variables] -> [Variables]
-multVars [] [] = []
-multVars as bs = tupleVars (zip as bs)
-
 multMonos :: Monomial -> Monomial -> Monomial
-multMonos a b = Mono ((coefficient a) * (coefficient b)) (multVars (variables a) (variables b))
+multMonos a b 
+        | null(variables a) && null(variables b) = Mono ((coefficient a) * (coefficient b)) []
+        | null(variables a) = Mono ((coefficient a) * (coefficient b)) (variables b)
+        | null(variables b) = Mono ((coefficient a) * (coefficient b)) (variables a)
+        | otherwise = Mono ((coefficient a) * (coefficient b)) (variables a ++ variables b)
+
+tryOut :: [Char] -> [Char] -> [(Char, Char)]
+tryOut a b = [(x,y) | x <- a, y <- b]
 
 auxMultPoly :: Polynomial -> Polynomial -> Polynomial
 auxMultPoly [] [] = []
 auxMultPoly a b = [ multMonos mono1 mono2 | mono1 <- a, mono2 <- b]
 
-
 multiplyPolynomials :: String -> String -> String
 multiplyPolynomials [] [] = []
-multiplyPolynomials str1 str2 = parsePolynomial2String (auxMultPoly (cleanUpPolynomial(string2Polynomial str1)) (cleanUpPolynomial(string2Polynomial str2)))
+multiplyPolynomials str1 str2 = parsePolynomial2String (cleanUpPolynomial(auxMultPoly (cleanUpPolynomial(string2Polynomial str1)) (cleanUpPolynomial(string2Polynomial str2))))
+
+--- d) derive polynomials ----------------------------
 
 
-
---- d) derivate poly -----------------------------------
-filter :: Char -> Polynomial -> Polynomial
-filter n [] = []
-filter n xs =  [mono | mono <- xs, auxFilterVar (variables mono) == True]
-
-
-auxFilterVar :: Char -> [Variables] -> Bool
-auxFilterVar n [] = False
-auxFilterVar n (v:var)
-                | variable v == n = True
-                | otherwise = auxFilterVar n var
-
-derive :: Char -> Polynomial -> Polynomial
-derive n [] = []
-derive n poly = [actualDerive mono | mono <- poly]
-
-actualDerive :: Char -> Monomial -> Monomial
-actualDerive n mono = [degree x | x <- variables mono, x == m]
-
---codigo emprestado
-deriveExpo
-        = head (dropWhile (\ a -> var a /= deriveVar) (expos term1))
-      newNumeric = termFloatSignal term1 * expoNum deriveExpo
-      nonDerivingTerms = [x | x <- expos term1, var x /= deriveVar]
+-- derivePolynomials :: Char -> String -> String
+-- derivePolynomials _ [] = []
 
 
 -------------------------------------------------------
